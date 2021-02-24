90b113ce9eb65b2bf159dc1235c16b5e3b2ed219
Yujun Liu <dreamzack@gmail.com>
2021-02-23 22:54:09 -0800

    added comments section

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,12 +5,19 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
+import { DiscussionEmbed } from "disqus-react"
+
+
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
+    const disqusConfig = {
+      shortname: process.env.GATSBY_DISQUS_NAME,
+      config: { identifier: post.frontmatter.title },
+    }
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -61,6 +68,7 @@ class BlogPostTemplate extends React.Component {
             )}
           </li>
         </ul>
+        <DiscussionEmbed {...disqusConfig} />
       </Layout>
     )
   }
